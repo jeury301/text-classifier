04a6d4f94bcae73475d8abc5c5f24573736a060b
Jeury Mejia <jeury.mejia@gmail.com>
2018-09-30 15:55:36 -0400

    Add samples for noise removal and lexicon normalization - stemming, finding roots of the word

---
+++ preprocessing/samples/lexicon_normalization.py
@@ -0,0 +1,32 @@
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+def stemming(word):
+    """Stemming aka stripping the suffixes (“ing”, “ly”, “es”, “s” etc)
+    from a word.
+
+    We make use of the PorterStemmer from nltk.
+    :param word: Word to stem.
+    :return: Stemmed word.
+    """
+    stem = PorterStemmer() # stemmer object
+    return stem.stem(word)
+
+def lemmatization(word):
+    """Obtaining the root form of the word.
+
+    We make use of the WordNetLemmatizer from nltk.
+    :param word: Word to be lemmatized aka to find root.
+    :return
+    """
+    lem = WordNetLemmatizer() # lemmatizer object
+    return lem.lemmatize(word, "v")
+
+if __name__ == '__main__':
+    while True:
+        words = input("> ").split()
+        stems = [stemming(word) for word in words] # finding stems
+        roots = [lemmatization(word) for word in words] # finding roots
+
+        print("stems", " ".join(stems))
+        print("roots", " ".join(roots))
