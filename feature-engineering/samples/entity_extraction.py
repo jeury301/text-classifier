32e89a3d7695084c10bfb08f02949f90831377cc
Jeury Mejia <jeury.mejia@gmail.com>
2018-09-30 22:08:14 -0400

    Start entity extraction

--- feature-engineering/samples/entity_extraction.py
+++ feature-engineering/samples/entity_extraction.py
@@ -1,5 +1,5 @@
-import gensim from gensim
-import corpora
+from gensim import models
+from gensim import corpora
 
 def topic_modeling(corpus):
     """Implementation of Latent Dirichlet Allocation (LDA). In a list of words.
@@ -11,7 +11,22 @@ def topic_modeling(corpus):
     :param corpus: List of sentences which are part of the text corpus.
     :return: A trained LDA model for topic identification.
     """
-    pass
+    # creating a dictionray from corpus
+    # this is a super powerful dictionary object from gensim corpora
+    dictionary = corpora.Dictionary(corpus)
+
+    # creating bag of words from corpus
+    
+
 
 if __name__ == '__main__':
-    pass
+    doc1 = ("Sugar is bad to consume. My sister likes to have sugar, "
+        "but not my father.")
+    doc2 = ("My father spends a lot of time driving my sister "
+        "around to dance practice.")
+    doc3 = ("Doctors suggest that driving may cause increased stress "
+        "and blood pressure.")
+    doc_complete = [doc1, doc2, doc3]
+    corpus = [doc.split() for doc in doc_complete]
+
+    topic_modeling(corpus)
